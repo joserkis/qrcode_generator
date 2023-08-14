ffac5a88ba28e3f2736ad0ac4c4015bae8a605a8
Jose Rankis <141494710+joserkis@users.noreply.github.com>
2023-08-14 13:57:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
 from editor import Editor
+from creator import Creator
 import sys
 import PyQt4
 
 if __name__ == '__main__':
     app = PyQt4.QtGui.QApplication(sys.argv)
     e = Editor()
-    app.exit(app.exec_())
+    app.exit(app.exec_(creator))
